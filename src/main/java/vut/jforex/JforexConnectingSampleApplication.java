009e4f3bfd6aea594f9b79f14aab6c117f4bebe0
Vu Tran <vuthh79@gmail.com>
2023-02-17 20:36:32 +0700

    Init spring project

---
+++ src/main/java/vut/jforex/JforexConnectingSampleApplication.java
@@ -0,0 +1,13 @@
+package vut.jforex;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JforexConnectingSampleApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JforexConnectingSampleApplication.class, args);
+    }
+
+}
---
+++ src/test/java/vut/jforex/JforexConnectingSampleApplicationTests.java
@@ -0,0 +1,13 @@
+package vut.jforex;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JforexConnectingSampleApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
